5ac16378a0cdf0a510dfa00d995ad62b22dda1c2
cponeill <caseyoneill78@hotmail.com>
2017-01-19 11:15:50 -0500

    Updating app with /about link.

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def action():
     return render_template("action.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
