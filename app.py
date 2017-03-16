f0af9a6e78a5ab5a62d7668ae7fdd609c4cec1f3
cponeill <caseyoneill78@hotmail.com>
2017-03-16 13:51:16 -0400

    Adding descriptive comments to server application.

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
+#! /usr/bin/env python3
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ Return home page template. """
     return render_template("index.html")
 
 @app.route('/action')
 def action():
+    """ Return action page template. """
     return render_template("action.html")
 
 @app.route('/about')
 def about():
+    """ Return about page template. """
     return render_template("about.html")
 
 if __name__ == '__main__':
